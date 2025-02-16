031433062064b65c212469dfda5b19798df6d36a
bjeker <rbieke1@gmail.com>
2025-02-15 21:17:05 -0600

    added random champion roll with counter

--- src/Game.jsx
+++ src/Game.jsx
@@ -2,6 +2,22 @@ import { useState } from "react"
 
 function Game() {
   const [count, setCount] = useState(0)
+  const [roll, setRoll] = useState(0)
+
+  const champions = [
+    { title: 'Jinx', id: 1},
+    { title: 'Jhin', id: 2},
+  ]
+
+  const listChampions = champions.map(champion =>
+    <li key={champion.id}>
+      {champion.title}
+    </li>
+  )
+
+  const [randChamp, setRandChamp] = useState(null)
+  
+  let random = Math.floor(Math.random() * listChampions.length)
 
   return (
     <>
@@ -16,6 +32,14 @@ function Game() {
       <br></br>
       <button onClick={() => setCount(0)}>
         Reset
+      </button>
+      <br></br>
+      <ul>{listChampions}</ul>
+      <button onClick={() => {
+        setRandChamp(champions[random].title);
+        setRoll((roll) => roll + 1);
+      }}>
+        Random Champion: {randChamp} Rolls: {roll}
       </button>
     </>
   )
