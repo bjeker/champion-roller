067b65dbe3ad51171aaaa549e4d744f66d6cda84
bjeker <rbieke1@gmail.com>
2025-02-15 20:39:02 -0600

    added buttons for count and reset

--- src/Game.jsx
+++ src/Game.jsx
@@ -1,4 +1,8 @@
+import { useState } from "react"
+
 function Game() {
+  const [count, setCount] = useState(0)
+
   return (
     <>
       <div>
@@ -6,6 +10,13 @@ function Game() {
           Hello World.
         </p>
       </div>
+      <button onClick={() => setCount((count) => count + 1)}>
+        Clicks: {count}
+      </button>
+      <br></br>
+      <button onClick={() => setCount(0)}>
+        Reset
+      </button>
     </>
   )
 }
